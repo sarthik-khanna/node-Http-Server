ed1490ef2bcf15b8f0cad1f5aafbd17d622561ee
khanna-sarthik <sarthikkhanna28@gmail.com>
2025-02-23 11:43:35 +0530

    first commit

--- index.js
+++ index.js
@@ -1,30 +1,55 @@
-// const http= require("https");
-
-// const myServer=http.createServer((req,res)=>{
-//     console.log("new Request received");
-//     res.end("hello from server");
-// });
-// const port=8000;
-// myServer.listen(port, ()=>console.log("server started"));
-
-const http= require("http");
+const http=require("http");
 const fs=require("fs");
+const url=require("url");
+// const myServer=http.createServer((req,res)=>{
+//     // if(res.url ==="/favicon.ico") return res.end();
+//     const log= `${Date.now()}: ${req.url} new request received\n`;
+//     const myUrl=url.parse(req.url,true);
+//     console.log(myUrl)
+//     fs.appendFile("./content.txt",log,(err,data)=>{
+//         // console.log("hello from server");
+//         switch(myUrl.pathname){
+//             case '/':
+//                 res.end("home page");
+//                 break;
+//                 case '/about':
+//                     const username=myUrl.query.myname;
+//                     res.end(`hi,${username}`);
+//                     break;
+//                     case '/contact':
+//                         res.end("9877878");
+//                         break;
+//                         default :
+//                         res.end("404");
+//         }
 
+//     })
+// })
+// myServer.listen(8000,()=>console.log("server start"));
 
- const myServer=http.createServer((req,res)=>{
-    const log=`${Date.now()}: ${req.url}req received\n`;
+const myServer=http.createServer((req,res)=>{
+    const log= `${Date.now()} ${req.url}: New request received\n`;
+    const Myurl=url.parse(req.url,true);
+    console.log(Myurl);
     fs.appendFile("./log.txt",log,(err,data)=>{
-switch(req.url){
-case '/':res.end("home page");
-break;
-case '/about' : res.end("i am sarthik khanna");
-break;
-case '/contact': res.end("99879798");
-break;
-default: res.end("404");
-}
-        // res.end("hello from server");
-    });
- });
+        console.log("helllo from server");
+        switch(Myurl.pathname){
+            case '/':
+                res.end("home page");
+                break;
+                case '/about':
+                    const username=Myurl.query.myname;
+                    res.end(`hi ${username}`);
+                    break;
+                    case '/contact':
+                        res.end("phone number");
+                        break;
+                        default:
+                            res.end("404");
+        }
+    })
 
- myServer.listen(8000,()=>console.log("server start"));
+})
+myServer.listen(8000,()=>console.log("server started"));
+
+
